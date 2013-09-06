2f6ba546100248c43d8611de1b3bec45c81e0fd1
Jeff Miller <jmiller@enova.com>
2013-09-05 20:33:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BetTrack::Application.config.secret_key_base = 'c28a24c4cc267b623a7deb68b122952e8387985988af717db01a7d8447e60128097a746d2005ddd3f9a1fcb88d79537442559206d5bec1a3f1e0f1d9133d91ed'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BetTrack::Application.config.session_store :cookie_store, key: '_BetTrack_session'
